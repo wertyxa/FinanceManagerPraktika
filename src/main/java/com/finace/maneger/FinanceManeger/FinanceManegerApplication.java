bcf415834f8a4c7ceda717028df560a6ba4b5cf4
wertyxa <wertyxa.main@gmail.com>
2020-03-24 16:06:25 +0200

    first comit

---
+++ src/main/java/com/finace/maneger/FinanceManeger/FinanceManegerApplication.java
@@ -0,0 +1,13 @@
+package com.finace.maneger.FinanceManeger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceManegerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceManegerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finace/maneger/FinanceManeger/FinanceManegerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finace.maneger.FinanceManeger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManegerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
